8df536b96a0691ec1f2b59893f89e13eb2a7de0a
kimjeayoon <kimjy9698@gmail.com>
2023-02-15 02:26:35 +0900

    gir push origin master

--- class/section11/11-06-google-login/backend/dist/apis/productsCategories/productsCategories.resolver.d.ts
+++
@@ -1,9 +0,0 @@
-import { ProductCategory } from './entities/productCategory.entity';
-import { ProductsCategoriesService } from './productsCategories.service';
-export declare class ProductsCategoriesResolver {
-    private readonly productCategoriesService;
-    constructor(productCategoriesService: ProductsCategoriesService);
-    createProductCategory(name: string): Promise<{
-        name: any;
-    } & ProductCategory>;
-}
